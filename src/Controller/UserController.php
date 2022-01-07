da8a89bf1d56d4ec9a1848ba185de46e6840e67a
Tex <julestexier@live.fr>
2022-01-07 13:52:00 +0100

    finishing save

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,9 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request; // Importation de la classe Request
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Form\UserType;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 
 class UserController extends AbstractController
@@ -18,8 +21,19 @@ class UserController extends AbstractController
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
+
+        $form-->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            echo '<script>alert("Adresse enregistrée. Merci et à bientôt ")</script>';
+            return $this->redirectToRoute('home');            
+        }
         return $this->render('user/index.html.twig', [
             'form' => $form->createView()
         ]);
         }
+
 }
